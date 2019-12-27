8ce076339cdb1a9ae9910771efd65fa4ed410bab
Dane Schmidt <daneschmidt@gmail.com>
2019-12-27 10:54:08 -0600

    setting up details page to start displaying individual info wired it up to the store

--- src/components/pages/Details/Details.js
+++ src/components/pages/Details/Details.js
@@ -1,4 +1,6 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
 
  class Details extends Component {
 
@@ -10,4 +12,4 @@ import React, { Component} from 'react';
      }
  }
 
- export default Details;
+ export default connect(mapStoreToProps)(Details);
