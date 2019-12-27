ca68b1f27867b9c9462c75ce24f7f368c87d25de
Dane Schmidt <daneschmidt@gmail.com>
2019-12-27 11:34:33 -0600

    trying to get just ONE movies details to appaer on details page

--- src/components/pages/Details/Details.js
+++ src/components/pages/Details/Details.js
@@ -1,15 +1,42 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
- class Details extends Component {
+class Details extends Component {
 
-     render(){
+    componentDidMount(title, description, name) {
+        this.props.dispatch({
+            type: 'GET_MOVIES',
+            payload: {
+                title,
+                description,
+                name,
+            }
+        });
+    }
 
-         return(
-             <div>YO WE GOT DEM DEETS</div>
-         )
-     }
- }
 
- export default connect(mapStoreToProps)(Details);
+    render() {
+
+        const movies = this.props.store.movies.map((item, index) => {
+
+            return <div key={index}>
+                <img src={item.poster} />
+                <h3>{item.title}</h3>
+                {item.description}
+                <h4>{item.name}</h4>
+            </div>
+        })
+
+        return (
+            <div>
+                <h2>YO WE GOT DEM DEETS</h2>
+                <table className='App'>
+                    <tbody>{movies}</tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default connect(mapStoreToProps)(Details);
